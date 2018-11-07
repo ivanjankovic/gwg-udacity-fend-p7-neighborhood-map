d97b57f8aec035ae9e88f0bac930d92bb81ecbba
ivan Jankovic <jankoemail@gmail.com>
2018-11-07 17:55:04 -0500

    Updates

---
+++ precache-manifest.c5cb7188ca66f4168a1f50a99ebebbc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d80820586d2a4b3dd45",
+    "url": "/gwg-udacity-fend-p7-neighborhood-map/static/js/runtime~main.6d808205.js"
+  },
+  {
+    "revision": "fd9d4d4d35c540095743",
+    "url": "/gwg-udacity-fend-p7-neighborhood-map/static/js/main.fd9d4d4d.chunk.js"
+  },
+  {
+    "revision": "529d9f4eb7f82faf7530",
+    "url": "/gwg-udacity-fend-p7-neighborhood-map/static/js/1.529d9f4e.chunk.js"
+  },
+  {
+    "revision": "fd9d4d4d35c540095743",
+    "url": "/gwg-udacity-fend-p7-neighborhood-map/static/css/main.57446ef3.chunk.css"
+  },
+  {
+    "revision": "83e5905eabea38b4800b5661ca0dbd86",
+    "url": "/gwg-udacity-fend-p7-neighborhood-map/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2dff3c816a9e29abf86902d00319961.js"
+  "/gwg-udacity-fend-p7-neighborhood-map/precache-manifest.c5cb7188ca66f4168a1f50a99ebebbc2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gwg-udacity-fend-p7-neighborhood-map/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
