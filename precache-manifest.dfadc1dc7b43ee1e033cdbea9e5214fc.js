909ff970c470d5b26c990ef22ca287dafd6ad793
ivan Jankovic <jankoemail@gmail.com>
2018-11-07 15:52:29 -0500

    Updates

---
+++ precache-manifest.dfadc1dc7b43ee1e033cdbea9e5214fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75be833635c02331d04f",
+    "url": "/static/js/main.75be8336.chunk.js"
+  },
+  {
+    "revision": "529d9f4eb7f82faf7530",
+    "url": "/static/js/1.529d9f4e.chunk.js"
+  },
+  {
+    "revision": "75be833635c02331d04f",
+    "url": "/static/css/main.57446ef3.chunk.css"
+  },
+  {
+    "revision": "671f1b29399e64db34b5718cb0615bf9",
+    "url": "/index.html"
+  }
+];
